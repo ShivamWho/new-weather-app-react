efaff8442b7f171a5c706c748fcf9b6f920306d8
Shivam Sindhi <shivam2094.be20@chitkara.edu.in>
2022-09-25 23:24:11 +0530

    added sounds

--- src/components/DetailsCard.js
+++ src/components/DetailsCard.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import moment from 'moment'
 import '../css/DetailsCard.css'
+import Sound from 'react-sound'
+import Clear from '../asset/sounds/clear.mp3'
+import Clouds from '../asset/sounds/clouds.mp3'
+import Rain from '../asset/sounds/rain.mp3'
 
 function DetailsCard({weather_icon, data}) {
   const {clouds, main, weather} = data.list[0]
+  var sound
+  if(weather[0].main === 'Clouds'){
+    sound = Clouds
+  }
+  else if(weather[0].main === 'Rain'){
+    sound = Rain
+  }
+  else{
+    sound = Clear
+  }
   return (
     <div className="details">
        <div className="clouds">
@@ -12,6 +26,11 @@ function DetailsCard({weather_icon, data}) {
             {weather[0].main}
             <img src={weather_icon} className="" alt="" />
            </div>
+           <Sound
+              url = {sound} 
+              playStatus = {Sound.status.PLAYING}
+              playFromPosition = {0}
+           />
            <p className="des">{weather[0].description}</p>
            <p className="time">{moment().format("dddd MMM YYYY")}</p>
        </div>
