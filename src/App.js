e02e4a91f1f4aa21e2a371c72a52cf66aa9b4d4c
Zulbukharov <l1pt0n1905@gmail.com>
2022-10-03 15:22:54 +0600

    fix: eslint warnings

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   const [weatherIcon, setWeatherIcon] = useState(`http://openweathermap.org/img/wn/10n@2x.png`)
 
   const handleChange = input => {
-    const {value} = input.target
+    const { value } = input.target
     setSearchTerm(value)
   }
 
@@ -28,10 +28,10 @@ function App() {
     let how_to_search = (typeof location === 'string') ? `q=${location}` : `lat=${location[0]}&lon=${location[1]}`
 
     try {
-      let res = await fetch(`${'http://api.openweathermap.org/data/2.5/forecast?'+how_to_search}
+      let res = await fetch(`${'http://api.openweathermap.org/data/2.5/forecast?' + how_to_search}
       &appid=${API_KEY}&units=metric&cnt=5&exclude=hourly,minutely`)
       let data = await res.json()
-      if(data.cod != 200) {
+      if (data.cod !== 200) {
         setNoData('Location Not Found')
         return
       }
@@ -44,14 +44,14 @@ function App() {
   }
 
   const myIP = (location) => {
-    const {latitude, longitude} = location.coords
+    const { latitude, longitude } = location.coords
     getWeather([latitude, longitude])
   }
 
   return (
     <div className="container">
-      <div className="blur" style={{top: '-10%', right: '0'}}></div>
-      <div className="blur" style={{top: '36%', left: '-6rem'}}></div>
+      <div className="blur" style={{ top: '-10%', right: '0' }}></div>
+      <div className="blur" style={{ top: '36%', left: '-6rem' }}></div>
       <div className="content">
         <div className="form-container">
           <div className="name">
@@ -65,36 +65,36 @@ function App() {
             <h2>The Only Weather App You Need !</h2>
             <hr />
             <form className="search-bar" noValidate onSubmit={handleSubmit}>
-              <input type="text" name="" id="" placeholder='#Explore ?' onChange={handleChange} required/>
+              <input type="text" name="" id="" placeholder='#Explore ?' onChange={handleChange} required />
               <button className="s-icon">
-                  <TbSearch 
-                    onClick={() => {
-                      navigator.geolocation.getCurrentPosition(myIP)
-                    }}
-                  />
+                <TbSearch
+                  onClick={() => {
+                    navigator.geolocation.getCurrentPosition(myIP)
+                  }}
+                />
               </button>
             </form>
           </div>
-          
+
         </div>
         <div className="info-container">
-          {weatherData.length === 0 ? 
-              <div className="nodata">
-                <h1>{noData}</h1>
-              </div> : 
-              <>
-                <h1>Today</h1>
-                <DetailsCard weather_icon={weatherIcon} data={weatherData} />
-                <h1 className="title">More On {city}</h1>
-                <ul className="summary">
-                  {weatherData.list.map((days, index) =>{
-                    if(index > 0){
-                      return (<SummaryCard key={index} day={days} />)
-                    }
-                  })}
-                </ul>
-              </>
-            }
+          {weatherData.length === 0 ?
+            <div className="nodata">
+              <h1>{noData}</h1>
+            </div> :
+            <>
+              <h1>Today</h1>
+              <DetailsCard weather_icon={weatherIcon} data={weatherData} />
+              <h1 className="title">More On {city}</h1>
+              <ul className="summary">
+                {weatherData.list.foreach((days, index) => {
+                  if (index > 0) {
+                    return (<SummaryCard key={index} day={days} />)
+                  }
+                })}
+              </ul>
+            </>
+          }
         </div>
       </div>
     </div>
